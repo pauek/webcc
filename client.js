82a88d078e9bf4a4f2f9c0544045a0e5d18364e2
Pau Fernández <pau.fernandez@upc.edu>
2011-03-31 17:04:42 +0200

    Making progress

--- client.js
+++ client.js
@@ -1,23 +1,63 @@
 
-function show_errors(errs) {
+var editor;
+
+function clear_errors() {
   $("#errors").html('');
+}
+
+var highlighted_item = null;
+
+function highlight_item(item) {
+  if (highlighted_item) {
+	 $(highlighted_item).css({ background: "none" });
+  }
+  $(item).css({ background: "red", });
+  highlighted_item = item;
+}
+
+function add_error(err) {
+  var patt = /^(\d+): ?error:/;
+  var result = err.match(patt);
+  var lineno;
+  if (result && result.length > 0) {
+	 lineno = Number(result[0]);
+  }
+  $('<pre>' + err + '</pre>')
+    .click(function () {
+		editor.scrollToLine(lineno, false);
+		highlight_item(this);
+	 })
+	 .appendTo("#errors");
+}
+
+function show_errors(errs) {
+  clear_errors();
   if (errs.length == 0) {
-	 $('<li>No errors</li>').appendTo('#errors');
+	 $("#errors").css({ height: 30 });
   } else {
+	 $("#errors").css({ height: "30%" });
 	 for (var i = 0; i < errs.length; i++) {
-		$('<li>' + errs[i] + '</li>').appendTo("#errors");
+		add_error(errs[i]);
 	 }
   }
+  var H = $("#errors").height();
+  $("#editor").css({ bottom: H });
 }
 
 function setup(remote) {
-  var editor = ace.edit("editor");
+  editor = ace.edit("editor");
   var CppMode = require("ace/mode/c_cpp").Mode;
   editor.getSession().setMode(new CppMode());
 
   $("#compile").click(function () {
     var code = editor.getSession().getDocument().getValue();
     remote.compile(code, show_errors);
+  });
+
+  $("#errors").resize(function () {
+	 var y = $("body").height() - $("#errors").height();
+    alert(y);
+	 $("#editor").css({ bottom: y });
   });
 }
 
