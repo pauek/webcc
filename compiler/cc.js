e56480947cf13bfb37937446dbe2846213b64d28
Pau Fernández <pau.fernandez@upc.edu>
2011-10-09 15:08:20 +0200

    cc.js acepta un argumento en el terminal

--- compiler/cc.js
+++ compiler/cc.js
@@ -5,7 +5,15 @@ var fs = require('fs');
 var cc = require('c++-parser');
 var ast = require('ast');
 
-fs.readFile('test.cc', 'utf-8', function (err, data) {
+if (process.argv.length < 3) {
+   console.log("usage: cc.js <input-file>");
+   process.exit(1);
+}
+
+var ccfile = process.argv[2];
+
+fs.readFile(ccfile, 'utf-8', function (err, data) {
+   console.log(data);
    var tree = cc.parse(data);
    tree.walk(ast.showTree);
 });
